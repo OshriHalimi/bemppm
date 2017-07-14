65b968e8c9d9a8755ecf3361231f0b2f17004918
MScroggs <matthew.w.scroggs@gmail.com>
2017-07-14 10:36:04 +0100

    Make better docs

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('../python/bempp'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -105,7 +105,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
